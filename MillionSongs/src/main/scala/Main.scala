b89e6a1bdc67746dfe3f925259b525cc8c015615
Mark Garner <ms675199@rams.colostate.edu>
2022-11-20 17:38:36 -0700

    implemented Read and Write CSV file

--- MillionSongs/src/main/scala/Main.scala
+++ MillionSongs/src/main/scala/Main.scala
@@ -1,29 +1,24 @@
 package src
 
 import src.Ignore.SparkSessionBuilder
-import src.Databases.MillionSongsDatabase
+// import src.Databases.MillionSongsDatabase
+import scala.collection.mutable.Map
+import org.apache.spark.sql.SparkSession
 
 object Main {
 
   def main(args: Array[String]) = {
     val sc = SparkSessionBuilder.getContext()
+    val sparkSession = SparkSessionBuilder.getSession()
 
-    // val database = new MillionSongsDatabase(args(1))
-    // database.output(sc, args(2) + "database")
+    // Read CSV File
+    val df = sparkSession.read
+      .option("header", "true")
+      .csv(args(0))
 
-    // val bufferedSource = io.Source.fromFile("/tmp/finance.csv")
-    // for (line <- bufferedSource.getLines) {
-    //     val cols = line.split(",").map(_.trim)
-    //     // do whatever you want with the columns here
-    //     println(s"${cols(0)}|${cols(1)}|${cols(2)}|${cols(3)}")
-    // }
-    // bufferedSource.close
-    // ------- Write Code Here --------
-    // val csvFile = sc.textFile(args(1))
-    // csvFile.saveAsTextFile(args(2) + "csvFile")
-
-    val titles = sc.textFile(args(1)).zipWithIndex().mapValues(x=>x+1).map(_.swap)
-    titles.saveAsTextFile(args(2) + "titles")
+    // Write CSV File
+    df.write.option("header", "true")
+   .csv(args(1) + "dataFrame")
   }
 }
   
